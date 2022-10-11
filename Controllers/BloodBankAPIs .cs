fd2290efcbebbf66354ee29f0280ef9603c92618
Ayamahmoud359 <101875618+Ayamahmoud359@users.noreply.github.com>
2022-10-11 19:48:23 +0200

    First Commit

---
+++ Controllers/BloodBankAPIs .cs
@@ -0,0 +1,31 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.WebUtilities;
+using System.Text;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace Controllers
+{
+
+    [ApiController]
+    [Route("api/[Controller]/[action]")]
+    public class BloodBankAPIsController
+    {
+        [HttpGet]
+        public void SayHI()
+        {
+
+        }
+    }
+    }
---
+++ Data/MarkatoooContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
+
+#nullable disable
+
+namespace BloodBank.Data
+{
+    public partial class BloodBankContext : DbContext
+    {
+        public BloodBankContext()
+        {
+        }
+
+        public BloodBankContext(DbContextOptions<BloodBankContext> options)
+            : base(options)
+        {
+        }
+       
+
+        //public virtual DbSet<PaymentGetway> PaymentGetways { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           
+            
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
